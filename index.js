b0baa19e3db25a030d6677a769d84a3d662b379b
Kyle Luck <luck.kyle@gmail.com>
2016-07-12 11:26:23 -0400

    defaulting to Lovies on Tuesdays

--- index.js
+++ index.js
@@ -14,5 +14,11 @@ var lunchOptions = [
 var lunchChoice = Math.floor(Math.random() * lunchOptions.length);
 
 module.exports = function() {
+	// if Tuesday, default to Lovies as Tuesday is 20% off
+	var d = new Date();
+	var today = d.getDay();
+	if (today === 2) {
+		return "It's Tuesday! Lovies it is!";
+	}
   return "Your choice for lunch is: " + lunchOptions[lunchChoice];
 };
